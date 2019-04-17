90664ba710ed684a05d27b5805551aad88b994f7
Jazilch <jzilch@hubspot.com>
2019-04-16 20:46:30 -0400

    adds header component

--- client/src/containers/RecipesContainer.js
+++ client/src/containers/RecipesContainer.js
@@ -2,33 +2,15 @@
 
 import React from 'react';
 import { Query } from 'react-apollo';
-
-import { Card, Col, Row, Empty, Spin } from 'antd';
-
+import RecipeCard from '../components/RecipeCard';
 import GetAllRecipes from '../graphql/queries/GetAllRecipes';
 
-const RecipesContainer = () => {
-  return (
-    <div>
-      <Query query={GetAllRecipes}>
-        {({ loading, data: { recipes } }) => (
-          <Row gutter={16}>
-            {!recipes && <Empty />}
-            {loading && <Spin />}
-            {recipes &&
-              recipes.map(recipe => (
-                <Col span={6} key={recipe.id}>
-                  <Card style={{ background: '#fff' }} title={recipe.title}>
-                    <p>{recipe.ingredients}</p>
-                    <p>{recipe.directions}</p>
-                  </Card>
-                </Col>
-              ))}
-          </Row>
-        )}
-      </Query>
-    </div>
-  );
-};
+const RecipesContainer = () => (
+  <Query query={GetAllRecipes}>
+    {({ loading, data: { recipes } }) => (
+      <RecipeCard loading={loading} recipes={recipes} />
+    )}
+  </Query>
+);
 
 export default RecipesContainer;
