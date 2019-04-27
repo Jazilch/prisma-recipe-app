248f319fd1553bc3e0e9f4616ce286d5cf619092
Jazilch <jzilch@hubspot.com>
2019-04-27 00:05:39 -0400

    adds react-test-renderer

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,8 +1,10 @@
+// 'use es6';
+
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 // import App from './App';
 
-// App test
+// // App test
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
 //   ReactDOM.render(<App />, div);
